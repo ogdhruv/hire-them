b0caca115e11db9e7f27d8501200508282916974
ogDhruv <dhruvrohilla.has@gmail.com>
2022-09-30 23:32:04 +0530

    :zap:added accounts app

--- base/settings.py
+++ base/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "blogs.apps.BlogsConfig",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom user model that we created
+AUTH_USER_MODEL = "accounts.CustomUser"
