1fec23d50e9f8c5f22917f86017946150f023018
ogDhruv <dhruvrohilla.has@gmail.com>
2022-10-08 20:47:03 +0530

    :tada: introduced new urlpatterns

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import BlogListView
+from .views import BlogDetailView, BlogListView, BlogUpdateView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blogs"),
+    path("post/<int:pk>/",BlogDetailView.as_view(),name="blog_detail"),
+    path("post/<int:pk>/udpate",BlogUpdateView.as_view(),name="blog_update"),
+
 ]
