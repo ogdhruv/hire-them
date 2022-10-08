b9199e3c21b19914cbc9700cd2712e5bd2cc6263
ogDhruv <dhruvrohilla.has@gmail.com>
2022-10-08 20:48:56 +0530

    :sparkles: added new class-based views

--- blogs/views.py
+++ blogs/views.py
@@ -11,3 +11,15 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name: str = "blogs/blogs_home.html"
+    context_object_name = "blogs"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name: str = "blogs/blog_detail.html"
+    context_object_name: str = "blog"
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name: str = "blogs/blog_update.html"
+    context_object_name: str = "blog"
+    fields = ["title","slug","body"]
