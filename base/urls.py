a6525d23de5dc67017bc38ca5f2b9104a8e059ca
ogDhruv <dhruvrohilla.has@gmail.com>
2022-10-13 13:30:45 +0530

    update: new reload url

--- base/urls.py
+++ base/urls.py
@@ -19,7 +19,8 @@ from django.conf.urls.static import static
 from base.settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
-    path("backend/", admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
+    path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("blogs/", include("blogs.urls")),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
