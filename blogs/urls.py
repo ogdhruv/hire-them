05a6c8b38c545e8f233bb706a586e0b4b311cbfa
ogDhruv <dhruvrohilla.has@gmail.com>
2022-10-14 18:05:41 +0530

    update: black formatting

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
-from .views import( BlogDeleteView, BlogDetailView, BlogListView, BlogUpdateView,BlogCreateView)
+from .views import (
+    BlogDeleteView,
+    BlogDetailView,
+    BlogListView,
+    BlogUpdateView,
+    BlogCreateView,
+)
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blogs"),
-    path("post/newblog/",BlogCreateView.as_view(),name="blog_create"),
-    path("post/<int:pk>/",BlogDetailView.as_view(),name="blog_detail"),
-    path("post/<int:pk>/udpate",BlogUpdateView.as_view(),name="blog_update"),
-    path("post/<int:pk>/delete",BlogDeleteView.as_view(),name="blog_delete"),
-
+    path("post/newblog/", BlogCreateView.as_view(), name="blog_create"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("post/<int:pk>/udpate", BlogUpdateView.as_view(), name="blog_update"),
+    path("post/<int:pk>/delete", BlogDeleteView.as_view(), name="blog_delete"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -9,28 +9,32 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 
-class BlogCreateView(LoginRequiredMixin,CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name: str = "blogs/blog_create.html"
-    fields = ["title","author","body"]
+    fields = ["title", "author", "body"]
+
 
 class BlogListView(ListView):
     model = Post
     template_name: str = "blogs/blogs_home.html"
     context_object_name = "blogs"
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name: str = "blogs/blog_detail.html"
     context_object_name: str = "blog"
 
-class BlogUpdateView(LoginRequiredMixin,UpdateView):
+
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name: str = "blogs/blog_update.html"
     context_object_name: str = "blog"
-    fields = ["title","body"]
+    fields = ["title", "body"]
 
-class BlogDeleteView(LoginRequiredMixin,DeleteView):
+
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name: str = "blogs/blog_delete.html"
     success_url = "blogs"
