a5e4ebd5d57f154b83919e1b8e72176ede0b9bd2
ogDhruv <dhruvrohilla.has@gmail.com>
2022-10-15 19:41:01 +0530

    add: basic view

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,13 @@
+from tempfile import TemporaryDirectory
+from unittest import TestProgram
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def dashboard(request):
+    return render(request, template_name="rooms/dashboard.html")
+
+
+def rooms(request):
+    return render(request, template_name="rooms/rooms.html")
