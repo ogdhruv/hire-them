48b03b2fcc7f00536830e6e027f8efe7865c17f7
ogDhruv <dhruvrohilla.has@gmail.com>
2022-10-15 19:39:42 +0530

    add: room datamodel to admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Room
 
 # Register your models here.
+@admin.register(Room)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["name", "updated", "created"]
+    search_fields = ["title", "description"]
+    # raw_id_fields = ["host"]
+    date_hierarchy = "created"
+    ordering = ["created", "updated"]
