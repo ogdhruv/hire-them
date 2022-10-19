fa49dfc2ae6eeabcfa3eba789feec31451e764b2
ogDhruv <dhruvrohilla.has@gmail.com>
2022-10-19 23:04:40 +0530

    :zap: added multiple urls

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import rooms,room
+from .views import createRoom, rooms, room, updateRoom,deleteRoom
 
 urlpatterns = [
     path("", view=rooms, name="rooms"),
-    path("?=<int:pk>/",view=room,name="room"),
+    path("?=<int:pk>/", view=room, name="room"),
+    path("create-room/", view=createRoom, name="create_room"),
+    path("?=<int:pk>/update-room/", view=updateRoom, name="update_room"),
+    path("?=<int:pk>/delete-room/", view=deleteRoom, name="delete_room")
 ]
