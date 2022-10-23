3839be3be9dff9d1b3108acdff18c8ad8600fd00
ogDhruv <dhruvrohilla.has@gmail.com>
2022-10-23 23:48:52 +0530

    change: black formatting

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import createRoom, rooms, room, updateRoom,deleteRoom
+from .views import createRoom, rooms, room, updateRoom, deleteRoom
 
 urlpatterns = [
     path("", view=rooms, name="rooms"),
     path("?=<int:pk>/", view=room, name="room"),
     path("create-room/", view=createRoom, name="create_room"),
     path("?=<int:pk>/update-room/", view=updateRoom, name="update_room"),
-    path("?=<int:pk>/delete-room/", view=deleteRoom, name="delete_room")
+    path("?=<int:pk>/delete-room/", view=deleteRoom, name="delete_room"),
 ]
