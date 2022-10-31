dbd5887e134a4db56cce66e42ece114ff41ddee9
ogDhruv <dhruvrohilla.has@gmail.com>
2022-10-31 23:36:07 +0530

    add: Job List view

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,13 @@
+from typing import List
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Job,Company
 
 # Create your views here.
 def dashboard(request):
     return render(request, "jobs/dashboard.html")
+
+class JobsView(ListView):
+    model = Job
+    template_name: str = "jobs/jobs.html"
+    context_object_name = "jobs"
