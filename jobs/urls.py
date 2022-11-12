17d191bdcda2e2234fdb484eddbbc83406acc2b3
ogDhruv <dhruvrohilla.has@gmail.com>
2022-11-12 22:07:32 +0530

    new: app jobs view and urls

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard,JobsView,JobsDetailView,JobsUpdateView
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("jobs/",JobsView.as_view(),name="jobs"),
+    path("jobs/f=<int:pk>",JobsDetailView.as_view(),name="jobview"),
+    path("jobs/update/f=<int:pk>",JobsUpdateView.as_view(),name="jobupdate"),
+    
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,6 @@
 from typing import List
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,UpdateView
 from .models import Job,Company
 
 # Create your views here.
@@ -11,3 +11,14 @@ class JobsView(ListView):
     model = Job
     template_name: str = "jobs/jobs.html"
     context_object_name = "jobs"
+
+class JobsDetailView(DetailView):
+    model = Job
+    template_name: str = "jobs/jobdetail.html"
+    context_object_name: str= "job"
+
+class JobsUpdateView(UpdateView):
+    model = Job
+    template_name: str = "jobs/jobupdate.html"
+    context_object_name: str = "job"
+    fields = ["title","description"]
